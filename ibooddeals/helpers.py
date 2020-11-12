e9b159b34bdfffd5e1f03a53b5f71dab32fdc346
Lucas dos Santos <lucas6eng@gmail.com>
2020-11-12 14:26:21 +0100

    Add scraping functions

--- ibooddeals/helpers.py
+++ ibooddeals/helpers.py
@@ -1,3 +1,12 @@
+"""
+Created on 12/11/2020
+
+@author: Lucas V. dos Santos
+"""
+from bs4 import BeautifulSoup
+import requests
+
+
 class WishList:
 
     def __init__(self, filename):
@@ -6,4 +15,24 @@ class WishList:
             for line in f:
                 self.items.append(line.strip())
 
+class Ibood:
+    # find_all
+    def __init__(self, url=None ):
+        self.url = 'https://ibood.nl' if url is None else url
 
+    def set_url(self, url):
+        self.url = url
+
+    def get_html(self):
+        source = requests.get(self.url).text
+        soup = BeautifulSoup(source, 'lxml')
+        return soup
+
+
+def main():
+    a = Ibood()
+    a.get_s()
+
+
+if __name__ == "__main__":
+    main()
--- ibooddeals/huntdeals.py
+++ ibooddeals/huntdeals.py
@@ -3,12 +3,48 @@ Created on 12/11/2020
 
 @author: Lucas V. dos Santos
 """
-from ibooddeals.helpers import WishList
 
-WishList
+from ibooddeals.helpers import WishList, Ibood
+import re
+import json
+class HuntDeals(Ibood):
+
+    def __init__(self, url=None):
+        super().__init__(url)
+
+    def get_product(self):
+        html = self.get_html()
+        for script in html.find_all('script'):
+            pattern = re.compile("product.push\(\s+(\{[\s\S]*),\s+\);\s+return product;")
+            json_data = script.find(text=pattern)
+            if json_data:
+                product = dict()
+                data = pattern.search(json_data).group(1)
+                for line in data.splitlines():
+                    if ':' in line:
+                        # print(line.strip().split(':'))
+                        category, value, *_ = line.strip().split(':')
+                        if value_strip:=re.search("'(.*?)'", value) :
+                            value = value_strip.group(1)
+                            product[category] = value
+        return product
+
+    def find_product_match(self, wishlist_file=None):
+
+        wish_list = WishList(wishlist_file).items
+
+        product = self.get_product()
+        print(product)
+        for item in wish_list:
+            if item.lower() in product['productName'].lower() or item.lower() in product['offerName'].lower():
+                print('Found')
+            else:
+                print('Not Found')
+
 
 def main():
-    pass
+    a =HuntDeals()
+    a.find_product_match('wishlist.txt')
 
 
 if __name__ == "__main__":
