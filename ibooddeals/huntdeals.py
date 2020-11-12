7ba20fd488f0761b7003daf54883b36a188b97e2
Lucas dos Santos <lucas6eng@gmail.com>
2020-11-12 15:28:40 +0100

    Add history file + Main body

--- ibooddeals/huntdeals.py
+++ ibooddeals/huntdeals.py
@@ -8,6 +8,8 @@ from ibooddeals.helpers import WishList, Ibood
 import re
 from notifypy import Notify
 import time
+import random
+import csv
 
 class HuntDeals(Ibood):
 
@@ -37,22 +39,47 @@ class HuntDeals(Ibood):
         product = self.get_product()
         for item in wish_list:
             if item.lower() in product['productName'].lower() or item.lower() in product['offerName'].lower():
+
                 notification = Notify()
                 time = re.search("(\d\d:\d\d:\d\d)", product['dealEndDateTime']).group(1)
                 message = f"Price:  {product['price']} EUR \nDiscount: {product.get('discount','-')}% \nEnds: {time}"
                 notification.title = product['productName']
                 notification.message = message
-
                 notification.send()
 
+        return product
 
+    def add_to_history(self, product):
+
+        with open('products_history.csv', 'a') as csvfile:
+            fieldnames = ['productID', 'productName', 'offerName', 'price', 'dealStartDateTime', 'dealEndDateTime']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            with open('products_history.csv', 'r') as f:
+
+                if sum(1 for _ in f) == 0:
+                    writer.writeheader()
+                f.seek(0)
+                reader = csv.DictReader(f)
+                APPEND = True
+                for line in reader:
+                    if line['productID'] == product['productID']:
+                        APPEND = False
+                        break
+
+                if APPEND:
+                    row_to_write = dict()
+                    for field in fieldnames:
+                        row_to_write[field] = product[field]
+                    writer.writerow(row_to_write)
 
 def main():
     RUNNING = True
-    # while RUNNING is True:
-    deal = HuntDeals()
-    deal.find_product_match('wishlist.txt')
-        # sleep
+    while RUNNING is True:
+        deal = HuntDeals()
+        product = deal.find_product_match('wishlist.txt')
+        deal.add_to_history(product)
+        time.sleep(random.randint(15, 30))
 
 
 if __name__ == "__main__":
